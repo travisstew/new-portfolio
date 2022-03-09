bce81db643a3bb3f807b7b8dab01538d371935ba
Travis <travisstew214@gmail.com>
2022-03-08 19:26:07 -0500

    contact form update

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 
+// router.get('/', (req,res)=>{
+//   res.render('home', {style:"/css/style.css",js:"/js/home.js" 
+//   });
+// });
+
 router.get('/', (req,res)=>{
-  res.render('home', {style:"/css/style.css",js:"/js/home.js" 
-  });
+  
+  res.render('home2', {style:'/css/style2.css'});
+
 });
 
 router.get('*', (req,res)=>{
